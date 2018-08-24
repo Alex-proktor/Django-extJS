a1966e030acde11f486f7caca271538a993b1b25
Alex-proktor <alex_tagan@mail.ru>
2018-08-24 07:51:18 +0300

    add file todo_detail.html

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from todo import views
 
 urlpatterns = [
     path(r'', views.todo_list, name='post_list'),
+    path(r'^todo/(?P<pk>\d+)/$', views.todo_detail, name='todo_detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 def todo_list(request):
     todo = Post.objects.all().order_by('published_date')
 
     return render(request, 'todo/todo_list.html', {'todo_list': todo})
+
+
+def todo_detail(request, pk):
+    todo = get_object_or_404(Post, pk=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
